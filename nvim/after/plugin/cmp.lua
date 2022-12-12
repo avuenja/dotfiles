7891202c23ccc5c5b1a6c649e0cd5ceffb8ed60a
Marcelo Pecin <celopeccin@gmail.com>
2022-12-12 22:31:26 +0100

    fix(nvim): fix cmp has_words_before

--- nvim/after/plugin/cmp.lua
+++ nvim/after/plugin/cmp.lua
@@ -9,6 +9,7 @@ local lspkind = require("lspkind")
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = "menuone,longest,preview"
 
+table.unpack = table.unpack or unpack -- 5.1 compatibility
 local has_words_before = function()
 	local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
 	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
