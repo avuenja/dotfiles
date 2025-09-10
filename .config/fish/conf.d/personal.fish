7c33dc658a409f6c25e5b93621ffbb6dc6672206
Marcelo Pecin <celopeccin@gmail.com>
2025-09-10 14:30:53 +0200

    feat: git log alias

--- .config/fish/conf.d/personal.fish
+++ .config/fish/conf.d/personal.fish
@@ -7,6 +7,8 @@ alias vim="nvim"
 
 alias :q="exit"
 
+alias glog="git log --graph --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%an%C(reset)%C(bold yellow)%d%C(reset) %C(dim white)- %s%C(reset)' --all"
+
 # Starship
 set -gx STARSHIP_CONFIG "$HOME/.config/starship/starship.toml"
 starship init fish | source
